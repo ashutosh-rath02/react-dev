b7450dd377251cadf71fc015a9ab049d5cdf3dd9
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-06-18 00:30:18 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,18 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "h1",
+    { id: "heading" },
+    React.createElement("span", null, "Hello World!")
+  ),
+  React.createElement("p", { id: "paragraph" }, "This is a paragraph."),
+]);
+
+const heading1 = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
